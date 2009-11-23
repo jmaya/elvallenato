03e5316e90f9a02eeaa27e87980e3d720d788f0d
=John Maya <jmaya@ewebhealth.com>
2009-11-23 12:39:18 -0500

    some search enhancements

--- lib/elvallenato/search.rb
+++ lib/elvallenato/search.rb
@@ -1,4 +1,48 @@
-class Search
+module ElVallenato
+  class Search
+    require 'CGI'
+    
+    def initialize(mode,query)
+      @mode = mode
+      @query = query
+    end
 
+    def url
+      if @query.nil? or @query == ""
+        "http://www.elvallenato.com/letras/busqueda.php?x=&busca=#{@mode}&image.x=8&image.y=6"
+      else
+        "http://www.elvallenato.com/letras/busqueda.php?x=#{CGI.escape(@query)}&busca=#{@mode}&image.x=12&image.y=11"
+      end
+    end
 
+    def next_url=(next_url)
+      @next_url = next_url
+    end
+
+    def next_url
+      partial_next = "http://www.elvallenato.com/letras/"
+      @doc.search("a").each do |d|
+        partial_next << d["href"] if d.inner_html == 'Siguiente >>'
+      end
+      partial_next == "http://www.elvallenato.com/letras/" ? '' : partial_next
+      #     @next_url
+    end
+
+    def load_content=(load_content)
+      @load_content = load_content
+      @doc = Hpricot(@load_content)
+    end
+
+    def load_content
+      @load_content
+    end
+    
+    def collect_links
+      @doc.search(".class10").collect {|e| e.at("a")["href"]}
+    end
+
+    def collect_lirics
+      collect_links.collect {|l| Letra.new(l)}
+    end
+  end
 end
--- spec/search_spec.rb
+++ spec/search_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Search do
+  before(:each) do
+    @s = Search.new("canta", "Diomedes")
+    @search_content = File.join(File.dirname(__FILE__), "fixtures", "diomedes_search.htm")
+    @s.load_content = (File.read(@search_content))
+  end
+  
   it "should state cantante compositor clave the type and the search" do
-    s = Search.new("Cantante", "Diomedes Diaz")
+    #http://www.elvallenato.com/letras/busqueda.php?x=&busca=canta&image.x=8&image.y=6
+    s = Search.new("canta", "Diomedes Diaz")
+  end
+
+  it "should generate the right url" do
+    s = Search.new("canta", "")
+    s.url.should eql "http://www.elvallenato.com/letras/busqueda.php?x=&busca=canta&image.x=8&image.y=6"
+  end
+
+  it "should generate the right url with query" do
+    s = Search.new("canta", "Diomedes")
+    s.url.should eql "http://www.elvallenato.com/letras/busqueda.php?x=Diomedes&busca=canta&image.x=12&image.y=11"
+  end
+
+  it "should deal with url query with spaces" do
+    s = Search.new("canta", "Diomedes Dias")
+    s.url.should eql "http://www.elvallenato.com/letras/busqueda.php?x=Diomedes+Dias&busca=canta&image.x=12&image.y=11"
+  end
+  it "should load content from a string" do
+    @s.load_content.size.should eql 54171
+  end
+  
+  it "should have a valid page" do
+    @s.next_url.should eql "http://www.elvallenato.com/letras/busqueda.php?x=Diomedes&busca=canta&pg=1"
+  end
+
+  it "should collect links" do
+    @s.collect_links.size.should eql 20
+  end
+
+  it "should generate liric objects from the links" do
+    @s.collect_lirics.size.should eql 20
   end
 end
